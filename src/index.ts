9b67eabea1dfe895ba55a5dabfaf461e04adf38f
trqdz <trqdz@hotmail.com>
2022-05-09 01:09:08 +0800

    fix import bug

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,44 @@
-import Button from './Button';
-import CheckBox from './CheckBox';
-import Drawer from './Drawer';
-import Dropdown from './Dropdown';
-import Input from './Input';
-import InputNumber from './InputNumber';
-import Message from './Message';
-import Modal from './Modal';
-import Radio from './Radio';
-import Scale from './Scale';
-import Select from './Select';
-import Spin from './Spin';
-import Switch from './Switch';
-import Table from './Table';
-import Tabs from './Tabs';
-import Tooltip from './Tooltip';
-import Tree from './Tree';
-import Trigger from './Trigger';
-import Upload from './Upload';
-import Card from './Card';
-import Collapse from './Collapse';
+export { default as Button } from './Button';
+export { default as CheckBox } from './CheckBox';
+export { default as Drawer } from './Drawer';
+export { default as Dropdown } from './Dropdown';
+export { default as Input } from './Input';
+export { default as InputNumber } from './InputNumber';
+export { default as Message } from './Message';
+export { default as Modal } from './Modal';
+export { default as Radio } from './Radio';
+export { default as Scale } from './Scale';
+export { default as Select } from './Select';
+export { default as Spin } from './Spin';
+export { default as Switch } from './Switch';
+export { default as Table } from './Table';
+export { default as Tabs } from './Tabs';
+export { default as Tooltip } from './Tooltip';
+export { default as Tree } from './Tree';
+export { default as Upload } from './Upload';
+export { default as Card } from './Card';
+export { default as Collapse } from './Collapse';
 
-export default {
-  Button,
-  CheckBox,
-  Drawer,
-  Dropdown,
-  Input,
-  InputNumber,
-  Message,
-  Modal,
-  Radio,
-  Scale,
-  Select,
-  Spin,
-  Switch,
-  Table,
-  Tabs,
-  Tooltip,
-  Tree,
-  Trigger,
-  Upload,
-  Card,
-  Collapse,
-};
+// export default {
+//   Button,
+//   CheckBox,
+//   Drawer,
+//   Dropdown,
+//   Input,
+//   InputNumber,
+//   Message,
+//   Modal,
+//   Radio,
+//   Scale,
+//   Select,
+//   Spin,
+//   Switch,
+//   Table,
+//   Tabs,
+//   Tooltip,
+//   Tree,
+//   Trigger,
+//   Upload,
+//   Card,
+//   Collapse,
+// };
