8789920e8a69551092a643d57680563930945343
trqdz <trqdz@hotmail.com>
2022-11-02 14:14:26 +0800

    动态引入css文件功能实现

--- config/color.js
+++ config/color.js
@@ -1,21 +1,25 @@
-const colorList = [
+const brandColorList = [
   {
     color: '#722ed2',
+    title: '紫色',
     name: 'purple',
     default: true,
   },
   {
     color: '#12c2c2',
+    title: '天蓝色',
     name: 'cyan',
   },
   {
     color: '#faae14',
+    title: '暮色',
     name: 'twilight',
   },
   {
     color: '#1990ff',
+    title: '天蓝色',
     name: 'blue',
   },
 ];
 
-module.exports = colorList;
+module.exports = brandColorList;
