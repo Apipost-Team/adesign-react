550fb44e804cd6b9b1b3af13070b983c98be74f8
fupeijie <1161353057@qq.com>
2023-01-09 17:07:10 +0800

    修改分页组件

--- src/Pagination/index.tsx
+++ src/Pagination/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 import Button from '../Button';
 import Select from '../Select';
+import Input from '../Select';
 import { PageProps } from './interface';
 import './index.less';
 
@@ -9,7 +10,7 @@ const Option = Select.Option;
 const pageSizeList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 const Pagination = (props: PageProps) => {
-  const { className, page = 1, pageSize = 10, total, onchange, onSizeChange } = props;
+  const { className, page = 1, pageSize = 10, total = 0, onchange, onSizeChange } = props;
 
   const [tempPage, setTempPage] = useState(1);
 
@@ -24,7 +25,6 @@ const Pagination = (props: PageProps) => {
         onChange={(size) => {
           onSizeChange && onSizeChange(size);
         }}
-        placement="auto"
         autoAdjustWidth={false}
       >
         <>
@@ -43,12 +43,12 @@ const Pagination = (props: PageProps) => {
       >
         上一页
       </Button>
-      <input
+      <Input
         value={tempPage}
         forceUseValue
-        onChange={(val) => {
+        onChange={(val: any) => {
           if (isNaN(+val)) return;
-          if (+val > total) val = total;
+          if (total && +val > total) val = total;
           if (val === '') val = 1;
 
           setTempPage(val);
@@ -57,7 +57,7 @@ const Pagination = (props: PageProps) => {
       <Button
         type="primary"
         disabled={page === total}
-        style={page === total ? { backgroundColor: 'rgba(0, 0, 0, 0.06)', color: '#999999' } : {}}
+        style={page === total || total < page ? { backgroundColor: 'rgba(0, 0, 0, 0.06)', color: '#999999' } : {}}
         onClick={() => onchange && onchange(onchange && onchange(page - 1))}
       >
         下一页
